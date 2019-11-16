47de0f277f4212696b2a292ff54fd5ac7eb3969e
itchengyalei <chengyalei000@163.com>
2019-11-16 19:24:08 +0800

    2

--- src/com/git/demo/Demo.java
+++ src/com/git/demo/Demo.java
@@ -4,5 +4,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("git helloworld");
 		System.out.println("gita程序元更新了代码,大家注意了");
+		System.out.println("gita程序元更新了代码,大家注意了");
+		System.out.println("gita程序元更新了代码,大家注意了");
 	}
 }

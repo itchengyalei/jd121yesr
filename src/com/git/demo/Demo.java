31ed9e4aa294996ee62998a437ebb78bf73fefef
itchengyalei <chengyalei000@163.com>
2019-11-16 20:34:53 +0800

    分支代码new

--- src/com/git/demo/Demo.java
+++ src/com/git/demo/Demo.java
@@ -8,5 +8,13 @@ public class Demo {
 		System.out.println("gita程序元更新了代码,大家注意了");
 		System.out.println("gita程序元更新了代码,大家注意了dfsfdfsdshha就哈哈充分图");
 		System.out.println("gita程序元更新了代码,大家注意了jfhdjskhfdkjh");
+		System.out.println("gita程序元更新了代码,大家注意了jfhdjskhfdkjh");
+		System.out.println("gita程序元更新了代码,大家注意了jfhdjskhfdkjh");
+		System.out.println("gita程序元更新了代码,大家注意了jfhdjskhfdkjh");
+		System.out.println("gita程序元更新了代码,大家注意了jfhdjskhfdkjh");
+		System.out.println("gita程序元更新了代码,大家注意了fenzhi分支");
+		System.out.println("gita程序元更新了代码,大家注意了fenzhi分支");
+		System.out.println("gita程序元更新了代码,大家注意了fenzhi分支");
+		System.out.println("gita程序元更新了代码,大家注意了fenzhi分支");
 	}
 }
